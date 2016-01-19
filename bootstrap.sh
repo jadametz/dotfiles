c4c5658c949326e6b05a7226e254930733c3cc1c
Jesse Adametz <jesseadametz@gmail.com>
2016-01-18 18:01:32 -0800

    bootstrap: fixed bugs that prevented proper boostrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,18 +3,8 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master;
-
 function bootstrap() {
-	rsync --exclude ".git/" \
-			--exclude ".DS_Store" \
-			--exclude "bootstrap.sh" \
-			--exclude "brew.sh" \
-			--exclude "new-machine.sh" \
-			--exclude "README.md" \
-			--exclude "LICENSE" \
-			-avh --no-perms . ~;
-
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "LICENSE" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
@@ -23,7 +13,7 @@ if [ "$1" == "--force" -o "$1" == "-f" ]; then
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
-	if [[ $RESPONSE =~ &[Yy]$ ]]; then
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		bootstrap;
 	fi;
 fi;
