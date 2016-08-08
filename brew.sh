75955df83b8642c5632857dca63b175a91b81821
Jesse Adametz <jesseadametz@gmail.com>
2016-08-08 12:09:49 -0700

    brew: removing horrible bash-completion2 package

--- brew.sh
+++ brew.sh
@@ -38,7 +38,6 @@ brew install gnu-sed --with-default-names
 # running `chsh`.
 brew install bash
 brew tap homebrew/versions
-brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
